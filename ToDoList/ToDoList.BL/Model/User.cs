1ea52b4dbd7a6e17138b10be4c5b77e8fc8a04d9
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-07-22 16:15:25 +0300

    Создана модель пользователя

---
+++ ToDoList/ToDoList.BL/Model/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.BL.Model
+{
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Номер объекта.
+        /// </summary>
+        private static int ObjectNumber = 0;
+
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        private int ID { get; set; }
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Пол.
+        /// </summary>
+        public Gender Gender { get; }
+
+        /// <summary>
+        /// Возраст.
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Создать нового пользователя
+        /// </summary>
+        /// <param name="name">Имя. </param>
+        /// <param name="gender">Пол. </param>
+        /// <param name="year">Возраст. </param>
+        public User(string name, Gender gender, int year)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(name));
+            }
+
+            if (year <= 0)
+            {
+                throw new ArgumentException("Некорректный возраст", nameof(year));
+            }
+
+            Gender = gender ?? throw new ArgumentNullException("Пол не может быть null", nameof(gender));
+
+            Year = year;
+            Name = name;
+            ObjectNumber++;
+            ID = ObjectNumber;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ToDoList/ToDoList.CMD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ToDoList.BL.Model;
+
+namespace ToDoList.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u1 = new User();
+            User u2 = new User();
+
+
+        }
+    }
+}
